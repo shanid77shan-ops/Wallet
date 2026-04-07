8aa192d8f42962169f1d605f8455477026821adc
shanid <shanid77shan@gmail.com>
2026-04-07 14:51:04 +0530

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { WalletProvider } from './context/WalletContext'
 import { CoinProvider } from './context/CoinContext'
+import { Web3Provider } from './providers/Web3Provider'
 import BottomNav from './components/BottomNav'
 import Home from './pages/Home'
 import Trending from './pages/Trending'
@@ -11,22 +12,24 @@ import './App.css'
 export default function App() {
   return (
     <BrowserRouter>
-      <WalletProvider>
-      <CoinProvider>
-      <div className="app-shell">
-        <main className="app-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/trending" element={<Trending />} />
-            <Route path="/trade" element={<Trade />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </main>
-        <BottomNav />
-      </div>
-      </CoinProvider>
-      </WalletProvider>
+      <Web3Provider>
+        <WalletProvider>
+          <CoinProvider>
+            <div className="app-shell">
+              <main className="app-content">
+                <Routes>
+                  <Route path="/"         element={<Home />}     />
+                  <Route path="/trending" element={<Trending />} />
+                  <Route path="/trade"    element={<Trade />}    />
+                  <Route path="/profile"  element={<Profile />}  />
+                  <Route path="*"         element={<Navigate to="/" replace />} />
+                </Routes>
+              </main>
+              <BottomNav />
+            </div>
+          </CoinProvider>
+        </WalletProvider>
+      </Web3Provider>
     </BrowserRouter>
   )
 }
